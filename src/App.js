72ab8938ee7cdac4e043f049f1d44a0d828b4d37
muluwork-shegaw <smslmuluwork@gmail.com>
2023-07-16 02:28:03 -0500

    complete sign-in and sign-up pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </>
   );
 }
 
